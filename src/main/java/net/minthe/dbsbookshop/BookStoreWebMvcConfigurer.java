317e82519abb16a317470da11e8a29cd15ebf090
Michael Kelley <michael@minthe.net>
2018-10-26 18:11:34 -0500

    Forbid guest users access to anything but login and registration.

---
+++ src/main/java/net/minthe/dbsbookshop/BookStoreWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package net.minthe.dbsbookshop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Michael Kelley on 10/26/2018
+ */
+@Configuration
+public class BookStoreWebMvcConfigurer implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    @Autowired
+    public BookStoreWebMvcConfigurer(LoginInterceptor loginInterceptor) {this.loginInterceptor = loginInterceptor;}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+}
+
---
+++ src/main/java/net/minthe/dbsbookshop/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package net.minthe.dbsbookshop;
+
+import net.minthe.dbsbookshop.repo.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Michael Kelley on 10/26/2018
+ */
+@Component
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    private final LoginService loginService;
+
+    @Autowired
+    public LoginInterceptor(LoginService loginService) {this.loginService = loginService;}
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!loginService.userLoggedIn() &&
+                !request.getServletPath().equals("/login") &&
+                !request.getServletPath().equals("/member/new")
+        ) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+}
