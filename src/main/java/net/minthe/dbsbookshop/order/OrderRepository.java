e84a8c8356590bd036b3fd3c6b90b232da5cf70e
Michael Kelley <michael@minthe.net>
2018-11-03 20:28:41 -0500

    Remove duplicates created with previous N+1 query fix.

--- src/main/java/net/minthe/dbsbookshop/order/OrderRepository.java
+++ src/main/java/net/minthe/dbsbookshop/order/OrderRepository.java
@@ -23,6 +23,6 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
             "WHERE ono = :ono", nativeQuery = true)
     BigDecimal getOrderTotal(@Param("ono") long ono);
 
-    @Query(value = "FROM Order as o join fetch o.orderDetailsList as detail join fetch detail.isbn where o.userid = :userid")
+    @Query(value = "select distinct o FROM Order as o join fetch o.orderDetailsList as detail join fetch detail.isbn where o.userid = :userid")
     List<Order> findByUseridWithDetails(@Param("userid") Member userid);
 }
